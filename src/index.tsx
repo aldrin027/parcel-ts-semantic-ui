f892d76d6b1abdff3fd8ff6eeabc0b0e12dade49
aldrin027 <aldrin.gauran@gmail.com>
2020-08-04 10:59:59 +0800

    Inital Commit parcel-reactTS-semantic-ui

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from'react';
+import ReactDOM from 'react-dom';
+// import 'semantic-ui-less/semantic.less';
+import './semantic-ui/semantic.less';
+
+import App from './App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
